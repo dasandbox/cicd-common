3381d752c56fade4e1926f654c707a426c492b36
dasandbox <87076889+dasandbox@users.noreply.github.com>
2021-11-15 15:54:29 -0500

    Updates to support baselines 560 and 561

--- am_run_local_etl.sh
+++ am_run_local_etl.sh
@@ -3,18 +3,26 @@
 me=$(basename $0)
 #echo "me: ${me} $@"
 
-# Use timestamp as default dataset/analysis id
+# The CI/CD pipeline will name the data/analysis same as the current DX file
+# This script can use a timestamp as the dataset/analysis if nothing supplied
 idtag=$(date +%Y%m%d_%H%M%S)
 if [ $# -eq 1 ]; then
   # Use user supplied name
   idtag="${1}"
 fi
 
+# The CI/CD pipeline will support TTWCS baselines 5.6.0 and 5.6.1
+baseline=TTWCS_Baseline_5_6_1
+if [ $# -eq 2 ]; then
+  # Use user supplied baseline
+  baseline="${2}"
+fi
+
 # Read config settings
 source ./readConfig.sh
 echo "AMSERVER=${AMSERVER}"
 
-url="http://${AMSERVER}/reaper/local_etl?etl_folder=./Tomahawk&analysis_id=${idtag}&catalog_file_dir=5.6.0_mm_catalog&data_file_dir=data"
+url="http://${AMSERVER}/reaper/local_etl?etl_folder=./Tomahawk/Baselines&analysis_id=${idtag}&catalog_file_dir=${baseline}/catalogs&data_file_dir=data"
 echo "url=${url}"
 
 # Make local_etl request to read data files
