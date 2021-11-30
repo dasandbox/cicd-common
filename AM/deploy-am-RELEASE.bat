9fbe5c2091d9648b2b31dda79bb1e3535fded670
DMA <danderson@idtus.com>
2021-11-30 15:46:59 +0000

    Update AM deploy script for new reaper_cache folder structure

--- AM/deploy-am-RELEASE.bat
+++ AM/deploy-am-RELEASE.bat
@@ -5,7 +5,7 @@ REM  ie: Right click on the ATRT_AM_4.37.x.zip archive -> 7-Zip -> Extract Here
 REM Then double-click on this batch file
 @ECHO OFF
 
-SET ETL="%~dp0\ATRT_AM\RELEASE\src\analysis\analysis.product\target\products\com.idtus.atrt.analysis.all-rest\win32\win32\x86_64\reaper_cache\etl\local"
+SET ETL="C:\Users\TTWCS\Programs\ATRT_AM\RELEASE\src\analysis\analysis.product\target\products\com.idtus.atrt.analysis.all-rest\win32\win32\x86_64\reaper_cache\etl\local\Tomahawk\Baselines"
 
 REM Remove old ATRT RELEASE folder
 RD /S /Q %~dp0\ATRT_AM\RELEASE
@@ -13,12 +13,8 @@ RD /S /Q %~dp0\ATRT_AM\RELEASE
 REM Copy new RELEASE folder to dest
 robocopy %~dp0\RELEASE "C:\Users\TTWCS\Programs\ATRT_AM\RELEASE" /S /E
 
-REM Make Tomahawk reaper_cache folders
-if not exist "%ETL%\Tomahawk" MD "%ETL%\Tomahawk"
-if not exist "%ETL%\Tomahawk\data" MD "%ETL%\Tomahawk\data"
-
-REM Copy catalog folder to reaper_cache
-robocopy RELEASE\5.6.0_mm_catalog "%ETL%\Tomahawk\5.6.0_mm_catalog" /S /E
+REM Make Tomahawk data folder
+if not exist "%ETL%\data" MD "%ETL%\data"
 
 REM Remove local RELEASE folder
 RD /S /Q %~dp0\RELEASE
